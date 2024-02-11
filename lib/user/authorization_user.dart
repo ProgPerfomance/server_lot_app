0040ce54f2aa5e31691ea79191baf06e3fffa36c
SmartCherry <97845331+ProgPerfomance@users.noreply.github.com>
2024-02-12 02:49:11 +0300

    Initial commit

--- lib/user/authorization_user.dart
+++ lib/user/authorization_user.dart
@@ -1,9 +1,16 @@
+import 'package:mysql_client/mysql_client.dart';
 import 'package:uuid/uuid.dart';
 
-import '../server_lot_app.dart';
 
 Future<void> registrationUser(request) async {
+var  sql = await MySQLConnection.createConnection(
+      host: 'localhost',
+      port: 3306,
+      userName: 'root',
+      password: '1234567890',
+      databaseName: 'lot');
+  await sql.connect();
   Uuid uuid = Uuid();
 String uuidString = uuid.v1();
-await  sql?.execute("insert into users (id, uuid, email, phone, first_name, last_name, password_hash) values (0, '$uuidString', '${request['email']}', null, null, null, '${request['password']}')");
+await  sql.execute("insert into users (id, uuid, email, phone, first_name, last_name, password_hash) values (0, '$uuidString', '${request['email']}', null, null, null, '${request['password']}')");
 }
